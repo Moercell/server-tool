79741387d531c4c209dc5cb5817e6742f658a21b
Marcel Pahnke <marcel.pahnke@gmail.com>
2019-10-29 18:42:49 +0100

    finish map

--- app.js
+++ app.js
@@ -1,14 +1,59 @@
 const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const readLastLines = require('read-last-lines');
+const geoip = require('geoip-lite');
 
-var screen = blessed.screen()
+const screen = blessed.screen()
+
+var newLog = "";
+var ip;
+var geo;
+var ipGroup = [];
+
+function isOdd(num) { return num % 2;}
+
+function getlog(log) {
+    //logs = log.split(/\r?\n/);
+    if (log != newLog) {
+        newLog = log;
+        ip = newLog.split(" ")[0];
+        ipGroup.push(ip);
+        if (ipGroup.length > 10) {
+            ipGroup.shift();
+        }
+    }
+    
+    //geo = geoip.lookup(ip).ll;
+}
+
+
+
 
 var grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
+var map = grid.set(0, 0, 6, 12, contrib.map, {label: 'access log | apache'});
+var log = grid.set(5, 0, 1, 12,contrib.log, { fg: "green", selectedFg: "green", label: 'access Log'})
 
 
-//grid.set(row, col, rowSpan, colSpan, obj, opts)
-var map = grid.set(0, 0, 5, 10, contrib.map, {label: 'access log | apache'})
-var box = grid.set(4, 0, 1, 10, blessed.box, {content: readLastLines.read('/var/log/apache2/access.log', 4)})
+let time = 0;
+setInterval(() => {
+    time ++
 
-screen.render()
+    readLastLines.read('/var/log/apache2/access.log', 1).then((lines) => getlog(lines));
+
+    log.log(String(newLog));    
+    for (let i = 0; i < ipGroup.length; i++) {
+        geo = geoip.lookup(ipGroup[i]).ll;
+        map.addMarker({"lon" : String(geo[1]), "lat" : String(geo[0]), color: "blue", char: "߉" })
+    }
+
+    if (isOdd(time) ) {
+        map.clearMarkers()
+    }
+
+
+    //var box = grid.set(6, 0, 3, 12, blessed.box, {label: 'debug box', content: String(logs)});
+
+    
+    screen.render();
+}, 800);
+
