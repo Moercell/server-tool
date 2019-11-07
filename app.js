f7f9b5e4b67c9188a66590dc5815d8fc2f0ea2a9
Marcel Pahnke <marcel.pahnke@gmail.com>
2019-11-08 00:03:20 +0100

    moving map with w-s-a-d

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const blessed = require('blessed');
 const contrib = require('blessed-contrib');
 const readLastLines = require('read-last-lines');
 const geoip = require('geoip-lite');
-const si = require('systeminformation');
-const exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
+//const si = require('systeminformation');
+
 
 
 const screen = blessed.screen({
     smartCSR: true
   });
+
 
 var newLog = "";
 var ip;
@@ -17,11 +17,18 @@ var geo;
 var ipGroup = [];
 var oldLog;
 var oldIps = [];
+var map;
+var log;
 
-function isOdd(num) { return num % 2;}
+
+var sLat = 0;
+var sLon = 0;
+var eLat = 180;
+var eLon = 360;
+
+
 
 function getlog(log) {
-    //logs = log.split(/\r?\n/);
     if (log !== newLog) {
         newLog = log;
         ip = newLog.split(" ")[0];
@@ -30,8 +37,6 @@ function getlog(log) {
             ipGroup.shift();
         }
     }
-    
-    //geo = geoip.lookup(ip).ll;
 }
 
 
@@ -43,22 +48,21 @@ function oldLogs(log) {
     }
 }
 
-// check "screen" size 
 
-
-//update grid
-
-
-//place elements
-
-var sLat = 0;
-var sLon = 0;
-var eLat = 0;
-var eLon = 0;
+function drawMap() {
+    map = grid.set(0, 0, 6, 12, contrib.map, {label: 'access log | apache', startLon: sLon, startLat: sLat, endLat: eLat, endLon: eLon});
+    return map
+}
+function drawLog() {
+    log = grid.set(5, 0, 1, 12, contrib.log, { fg: "green", selectedFg: "green", label: 'access Log'});
+    return log
+}
 
 var grid = new contrib.grid({rows: 12, cols: 12, screen: screen});
 
-var log = grid.set(5, 0, 1, 12, contrib.log, { fg: "green", selectedFg: "green", label: 'access Log'});
+drawMap();
+drawLog();
+
 var donut = grid.set(6, 0, 2, 6, contrib.donut, {
 	label: 'temp',
 	radius: 8,
@@ -72,95 +76,66 @@ var donut = grid.set(6, 0, 2, 6, contrib.donut, {
 
   // get keypress
   screen.on('keypress', function(key) {
-
-    // exit
-    if (key == "q") {
-        process.exit();
-    }
-
-    // move map ?
-
-    //europe
-    if (key == 'e') {
-        sLat = 118;
-        sLon = 159;
-        eLat = 161;
-        eLon = 241;
-    }
-
-    //zoom
-    if (key == 'x') {
-        sLat ++
-        sLon ++
-    }
-    if (key == 'y') {
-        sLat --
-        sLon --
-    }
-    //move?
-    if (key == 'm') {
-        eLat ++
-        eLon ++
-    }
-    if (key == 'n') {
-        eLat --
-        eLon --
-    }
-    //lon 
-    if (key == 'a') {
-        sLat --;
-    }
-    if (key == 'd') {
-        sLat ++;
-    }
-    //lat
-    if (key == 'w') {
-        sLon ++;
-    }
-    if (key == 's') {
-        sLon --;
-    }
-        //lon 
-        if (key == 'j') {
-            eLat --;
-        }
-        if (key == 'l') {
-            eLat ++;
-        }
-        //lat
-        if (key == 'i') {
+    switch (key) {
+        case 'q':
+            process.exit();
+            break;
+        case 'e':
+            sLat = 110;
+            sLon = 144;
+            eLat = 161;
+            eLon = 246;
+            break
+        case 'r':
+            sLat = 0;
+            sLon = 0;
+            eLat = 180;
+            eLon = 360;
+            break
+        case 'd':
+            sLon ++;
             eLon ++;
-        }
-        if (key == 'k') {
+            break
+        case 's':
+            sLat --;
+            eLat --;
+            break
+        case 'a':
+            sLon --;
             eLon --;
-        }
-    //log
-    if (key == 'c') {
-      console.log('slon: ' + sLon, 'slat: ' + sLat);
-      console.log('elon: ' + eLon, 'elat: ' + eLat);  
+            break
+        case 'w':
+            sLat ++;
+            eLat ++;
+            break
+        case 'x':
+            sLat ++
+            sLon += 2
+            break
+        case 'y':
+            sLat --
+            sLon -= 2
+            break
+        case 'c':
+            console.log('slon: ' + sLon, 'slat: ' + sLat);
+            console.log('elon: ' + eLon, 'elat: ' + eLat);
+        default:
+            break;
     }
 
-    if (key == "f") {
-        fan = exec("fanup", function(err, stdout, stderr) {
-            if (err) {
-              console.log(err)
-            }
-            console.log(stdout);
-          });
-          
-          fan.on('exit', function (code) {
-            // exit code is code
-          });
-    }
+    drawMap();
+    drawLog();
   });
 
 readLastLines.read('/var/log/apache2/access.log', 15).then((lines) => oldLogs(lines));
 
-
+function isOdd(num) { return num % 2;}
 let time = 0;
 setInterval(() => {
-    var map = grid.set(0, 0, 6, 12, contrib.map, {label: 'access log | apache', startLon: sLon, startLat: sLat, endLat: eLat, endLon: eLon});
     time ++;
+    if (time > 10) {
+        time = 0;
+    }
     readLastLines.read('/var/log/apache2/access.log', 1).then((lines) => getlog(lines));
     log.log(String(newLog));    
     for (let i = 0; i < ipGroup.length; i++) {
@@ -170,14 +145,12 @@ setInterval(() => {
         } catch (error) {
             //console.log(error);
         }
-        
         let amount = 0;
         for (let j = 0; j < ipGroup.length; j++) {
             if (ipGroup[i] == ipGroup[j]) {
                 amount ++;
             }
         }
-
         map.addMarker({"lon" : String(geo[1]), "lat" : String(geo[0]), color: "blue", char: String(amount) });
     }
 
@@ -198,18 +171,7 @@ setInterval(() => {
         ]);
     }
 
-    getCpu();
-
-
-    
-
-    temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
-
-    temp.stdout.on('data', function(data) {
-        console.log('Result: ' + data/1000 + ' degrees Celcius');
-    });
-
-    //var box = grid.set(6, 6, 3, 6, blessed.box, {label: 'debug box', content: String(cpuTemp)});
+   // getCpu();
 
     screen.render();
 }, 800);
