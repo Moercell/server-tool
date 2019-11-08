8e874128fb43720fed3b1872ff21df1bf7ab3aaf
Marcel Pahnke <marcel.pahnke@gmail.com>
2019-11-08 15:52:54 +0100

    new temp test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const readLastLines = require('read-last-lines');
 const geoip = require('geoip-lite');
 const si = require('systeminformation');
 const exec = require('child_process').exec;
-
+var spawn = require('child_process').spawn;
 
 // uptime
 si.getDynamicData(function(data) {
@@ -12,7 +12,7 @@ si.getDynamicData(function(data) {
     days = Math.floor(((uptime / 60) / 60)/ 24);
     hours = Math.floor((uptime / 60 / 60) - (days * 24));
     minutes =  Math.floor((uptime / 60) - (days * 24 * 60 + hours * 60));
-    console.log(days, 'days', hours, 'hours', minutes, 'minutes');
+    console.log(days + ' d', hours + ' h', minutes + ' m');
 
 })
 
@@ -22,7 +22,14 @@ si.currentLoad(function(data) {
 })
 
 setInterval(() => {
-    si.networkStats(function(data) {
-        console.log(data[0].operstate);
-    })
+
+
+    
+
+    temp = spawn('cat', ['/sys/class/thermal/thermal_zone0/temp']);
+
+    temp.stdout.on('data', function(data) {
+            console.log('Result: ' + data/1000 + ' degrees Celcius');
+    });
+
 }, 500);
