aa6dd43e24556c6ffc968ddaf2fa8e69ae033fb5
Marcel Pahnke <marcel.pahnke@gmail.com>
2019-11-09 01:18:48 +0100

    typo

--- app.js
+++ app.js
@@ -93,7 +93,7 @@ si.currentLoad(function(data) {
             lable: 'cpu' + i, 
             x: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
             y: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-            style: { line: color[i]}
+            style: { line: colors[i]}
         })
         //console.log(data.cpus[i].load)
   }
